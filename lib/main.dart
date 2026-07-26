a6223f37a54ed8aea66ee9f0b614bb1640f32a6d
yayo <vladislavlesuk@MacBook-Air-yayo.local>
2026-07-26 07:17:29 +0300

    Fix Windows SQLite initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 export 'app/app.dart';
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import 'app/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
 
   runApp(const RossTabakApp());
 }
